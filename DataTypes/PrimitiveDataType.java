041a58af35c717e106186dc2f5a2e636ed0f8542
JyothiSai1102 <jyothisai1102@gmail.com>
2023-08-03 22:37:53 +0530

    Create PrimitiveDataType.java
    Types of primitive datatypes

---
+++ DataTypes/PrimitiveDataType.java
@@ -0,0 +1,22 @@
+package com.eclipse;
+
+public class PrimitiveDataType {
+	public static void main(String[] args) {
+		boolean bool=true;
+		char ch ='$';
+		byte b= 10;
+		short s=10;
+		int num=45;
+		long l=9000092577L;
+		float f=1.44F;
+		double d=20.00;
+        System.out.println(bool);
+        System.out.println(ch);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+	}
+}
